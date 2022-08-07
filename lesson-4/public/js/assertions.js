da48ba3995f31731e1a87849fdc7c9b9873fbb7a
ViliusJur <p.vilius.j@gmail.com>
2022-08-07 23:49:06 +0300

    feat: assertions tasks done

--- lesson-4/public/js/assertions.js
+++ lesson-4/public/js/assertions.js
@@ -1,3 +1,47 @@
 "use strict";
-console.log('assertions');
+console.group('Assertions - užduotys');
+{
+    const stringVariable = 'paspausta!';
+    const divVariable = '&#9726;';
+    const NumberVariable = 20;
+    console.group('1. Sukurkite HTML mygtuką ir uždėkite jam įvykio klausiklį, jog paspaudus, būt spausdinamas tekstas "paspausta!"');
+    {
+        const bodyHtml = document.querySelector('body');
+        const button = document.createElement('button');
+        bodyHtml?.append(button);
+        button.setAttribute('id', 'button');
+        const buttonElement = document.getElementById('button');
+        buttonElement.innerHTML = 'CLICK ME';
+        buttonElement.onclick = (e) => {
+            e.preventDefault();
+            const paragraphElement = document.createElement('p');
+            bodyHtml?.append(paragraphElement);
+            paragraphElement.innerHTML = `${stringVariable}`;
+        };
+    }
+    console.groupEnd();
+    console.group('2. Sukurkite <div> elementą ir papildikyte jo turinį mažu kvadaratėliu kiekvieną kart, kuomet paspaudžiamas [1.] mygtukas');
+    {
+        const selectButton = document.getElementById('divbutton');
+        selectButton.onclick = (e) => {
+            e.preventDefault();
+            const selectDiv = document.getElementById('divas');
+            selectDiv.innerHTML += `${divVariable}`;
+        };
+    }
+    console.groupEnd();
+    console.group('3. Sukurkite <p> elementą kuriame spausdinsite skaičių - kiek kvadratėlių yra [2.] konteineryje');
+    {
+        const selectPrgh = document.getElementById('kvadrateliai');
+        let sqCount = 0;
+        const inSqCount = () => {
+            sqCount++;
+            selectPrgh.innerHTML = `Kvadratuku skaičius -> ${sqCount}`;
+        };
+        const selectButton = document.getElementById('divbutton');
+        selectButton.addEventListener('click', inSqCount);
+    }
+    console.groupEnd();
+}
+console.groupEnd();
 //# sourceMappingURL=assertions.js.map
--- lesson-4/src/assertions.ts
+++ lesson-4/src/assertions.ts
@@ -1,1 +1,58 @@
-console.log('assertions');
+/* eslint-disable no-lone-blocks */
+// Alikite užduotis, ir atspausdinkite savo sprendimo pavyzdžius užduoties blokuose
+console.group('Assertions - užduotys');
+{
+  // ↓↓↓↓ Tipus ir užduotims bendrus kintamuosius apraškite čia ↓↓↓↓
+    type SomeType = string | number
+    type EventHandler = (e: MouseEvent) => void;
+
+    const stringVariable: SomeType = 'paspausta!' as string;
+    const divVariable: SomeType = '&#9726;' as string;
+    const NumberVariable: SomeType = 20 as number;
+    // ↑↑↑↑ Tipus ir užduotims bendrus kintamuosius apraškite čia ↑↑↑↑
+
+    console.group('1. Sukurkite HTML mygtuką ir uždėkite jam įvykio klausiklį, jog paspaudus, būt spausdinamas tekstas "paspausta!"');
+    {
+      const bodyHtml = document.querySelector('body');
+      const button = document.createElement('button') as HTMLButtonElement;
+      bodyHtml?.append(button);
+      button.setAttribute('id', 'button');
+      const buttonElement: HTMLElement = document.getElementById('button') as HTMLElement;
+      buttonElement.innerHTML = 'CLICK ME';
+
+      buttonElement.onclick = (e) => {
+        e.preventDefault();
+        const paragraphElement = document.createElement('p');
+        bodyHtml?.append(paragraphElement);
+        paragraphElement.innerHTML = `${stringVariable}`;
+      };
+    }
+    console.groupEnd();
+
+    console.group('2. Sukurkite <div> elementą ir papildikyte jo turinį mažu kvadaratėliu kiekvieną kart, kuomet paspaudžiamas [1.] mygtukas');
+    {
+      const selectButton: HTMLButtonElement = document.getElementById('divbutton') as HTMLButtonElement;
+      selectButton.onclick = (e) => {
+        e.preventDefault();
+        const selectDiv: HTMLDivElement = document.getElementById('divas') as HTMLDivElement;
+        selectDiv.innerHTML += `${divVariable}`;
+      };
+    }
+    console.groupEnd();
+
+    console.group('3. Sukurkite <p> elementą kuriame spausdinsite skaičių - kiek kvadratėlių yra [2.] konteineryje');
+    {
+      const selectPrgh: HTMLParagraphElement = document.getElementById('kvadrateliai') as HTMLParagraphElement;
+
+      let sqCount: number = 0;
+      const inSqCount: EventHandler = () => {
+        // eslint-disable-next-line no-plusplus
+        sqCount++;
+        selectPrgh.innerHTML = `Kvadratuku skaičius -> ${sqCount}`;
+      };
+      const selectButton: HTMLButtonElement = document.getElementById('divbutton') as HTMLButtonElement;
+      selectButton.addEventListener('click', inSqCount);
+    }
+    console.groupEnd();
+}
+console.groupEnd();
