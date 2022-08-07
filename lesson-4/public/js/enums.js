f57c4c619726b80404d482462a84af595ca12bd5
ViliusJur <p.vilius.j@gmail.com>
2022-08-08 00:37:52 +0300

    feat: enums tasks done

--- lesson-4/public/js/enums.js
+++ lesson-4/public/js/enums.js
@@ -1,3 +1,64 @@
 "use strict";
-console.log('enums');
+console.group('Enums - užduotys');
+{
+    let LtCities;
+    (function (LtCities) {
+        LtCities["Vilnius"] = "Vilnius";
+        LtCities["Kaunas"] = "Kaunas";
+        LtCities["Klaip\u0117da"] = "Klaip\u0117da";
+        LtCities["\u0160iauliai"] = "\u0160iauliai";
+        LtCities["Panev\u0117\u017Eys"] = "Panev\u0117\u017Eys";
+    })(LtCities || (LtCities = {}));
+    let TopPopulationCountries;
+    (function (TopPopulationCountries) {
+        TopPopulationCountries[TopPopulationCountries["China"] = 0] = "China";
+        TopPopulationCountries[TopPopulationCountries["USA"] = 1] = "USA";
+        TopPopulationCountries[TopPopulationCountries["Indonesia"] = 2] = "Indonesia";
+        TopPopulationCountries[TopPopulationCountries["Pakistan"] = 3] = "Pakistan";
+        TopPopulationCountries[TopPopulationCountries["Brazli"] = 4] = "Brazli";
+    })(TopPopulationCountries || (TopPopulationCountries = {}));
+    let TopGdpCountries;
+    (function (TopGdpCountries) {
+        TopGdpCountries[TopGdpCountries["USA"] = 1] = "USA";
+        TopGdpCountries[TopGdpCountries["China"] = 2] = "China";
+        TopGdpCountries[TopGdpCountries["Japan"] = 3] = "Japan";
+        TopGdpCountries[TopGdpCountries["Germany"] = 4] = "Germany";
+        TopGdpCountries[TopGdpCountries["UK"] = 5] = "UK";
+    })(TopGdpCountries || (TopGdpCountries = {}));
+    console.group('1. Sukurkite 5 didžiausių Lietuvos miestų išvardinimą.');
+    {
+        const city1 = LtCities.Vilnius;
+        const city2 = LtCities.Kaunas;
+        const city3 = LtCities.Klaipėda;
+        const city4 = LtCities.Šiauliai;
+        const city5 = LtCities.Panevėžys;
+        console.log(city1, city2, city3, city4, city5);
+    }
+    console.groupEnd();
+    console.group('2. Sukurkite 5 šalių su didžiausiu gyventojų skaičiumi skaitinį išvardinimą.');
+    {
+        const topPopulationCountry = [
+            TopPopulationCountries.China,
+            TopPopulationCountries.USA,
+            TopPopulationCountries.Indonesia,
+            TopPopulationCountries.Pakistan,
+            TopPopulationCountries.Brazli,
+        ];
+        console.log(topPopulationCountry);
+    }
+    console.groupEnd();
+    console.group('3. Sukurkite 5 šalių su didžiausiu BVP skaitinį išvardinimą pradedant "1".');
+    {
+        const topGdpCountries = [
+            TopGdpCountries.USA,
+            TopGdpCountries.China,
+            TopGdpCountries.Japan,
+            TopGdpCountries.Germany,
+            TopGdpCountries.UK,
+        ];
+        console.log(topGdpCountries);
+    }
+    console.groupEnd();
+}
+console.groupEnd();
 //# sourceMappingURL=enums.js.map
--- lesson-4/src/enums.ts
+++ lesson-4/src/enums.ts
@@ -1,1 +1,67 @@
-console.log('enums');
+/* eslint-disable no-lone-blocks */
+console.group('Enums - užduotys');
+{
+    enum LtCities {
+        Vilnius = 'Vilnius',
+        Kaunas = 'Kaunas',
+        Klaipėda = 'Klaipėda',
+        Šiauliai = 'Šiauliai',
+        Panevėžys = 'Panevėžys'
+
+    }
+
+    enum TopPopulationCountries {
+        China,
+        USA,
+        Indonesia,
+        Pakistan,
+        Brazli
+    }
+
+    enum TopGdpCountries {
+        USA = 1,
+        China,
+        Japan,
+        Germany,
+        UK
+    }
+
+    console.group('1. Sukurkite 5 didžiausių Lietuvos miestų išvardinimą.');
+    {
+      const city1: LtCities = LtCities.Vilnius;
+      const city2: LtCities = LtCities.Kaunas;
+      const city3: LtCities = LtCities.Klaipėda;
+      const city4: LtCities = LtCities.Šiauliai;
+      const city5 = LtCities.Panevėžys;
+
+      console.log(city1, city2, city3, city4, city5);
+    }
+    console.groupEnd();
+
+    console.group('2. Sukurkite 5 šalių su didžiausiu gyventojų skaičiumi skaitinį išvardinimą.');
+    {
+      const topPopulationCountry: TopPopulationCountries[] = [
+        TopPopulationCountries.China,
+        TopPopulationCountries.USA,
+        TopPopulationCountries.Indonesia,
+        TopPopulationCountries.Pakistan,
+        TopPopulationCountries.Brazli,
+      ];
+      console.log(topPopulationCountry);
+    }
+    console.groupEnd();
+
+    console.group('3. Sukurkite 5 šalių su didžiausiu BVP skaitinį išvardinimą pradedant "1".');
+    {
+      const topGdpCountries: TopGdpCountries[] = [
+        TopGdpCountries.USA,
+        TopGdpCountries.China,
+        TopGdpCountries.Japan,
+        TopGdpCountries.Germany,
+        TopGdpCountries.UK,
+      ];
+      console.log(topGdpCountries);
+    }
+    console.groupEnd();
+}
+console.groupEnd();
