c9be4af89dcc0f4de42faaf8af72304b0b12473a
ViliusJur <107909725+ViliusJur@users.noreply.github.com>
2022-08-28 22:57:03 +0300

    feat: 12 and 13 homework

--- lesson-12/src/components/app.ts
+++ lesson-12/src/components/app.ts
@@ -1,8 +1,18 @@
+import Table from './table';
+import cars from '../data/cars';
+import brands from '../data/brands';
+import models from '../data/models';
+import CarsCollection from '../helpers/cars-collection';
+import stringifyProps from '../helpers/stingify-object';
+
 class App {
   private htmlElement: HTMLElement;
 
+  private carsCollection: CarsCollection;
+
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
+    this.carsCollection = new CarsCollection({ cars, brands, models });
 
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
@@ -10,9 +20,21 @@ class App {
   }
 
   initialize = (): void => {
+    const carTable = new Table({
+      title: 'Visi automobiliai',
+      columns: {
+        id: 'Id',
+        brand: 'Markė',
+        model: 'Modelis',
+        price: 'Kaina',
+        year: 'Metai',
+      },
+      rowsData: this.carsCollection.all.map(stringifyProps),
+    });
+
     const container = document.createElement('div');
     container.className = 'container my-5';
-    container.innerHTML = 'Laukiu kol būsiu sukurtas';
+    container.appendChild(carTable.htmlElement);
 
     this.htmlElement.append(container);
   };
